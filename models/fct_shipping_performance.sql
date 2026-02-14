becf3f390fbe352b9509c146b8266d57ad69426a
Dalbert Onyebuchi <dalbertonyebuchi@gmail.com>
2026-02-14 22:10:00 +0000

    Build Bronze, SIlver and Gold Layers

--- models/fct_shipping_performance.sql
+++ models/fct_shipping_performance.sql
@@ -1,18 +1,33 @@
-{{ config(
-    materialized='table',
-    catalog='workspace',
-    schema='supply_chain_analytics'
-) }}
+{{ config(materialized='table') }}
 
 with shipments as (
-  select * from {{ ref('stg_shipments') }}
+    select * from {{ ref('stg_shipments') }}
 )
 
-select 
-    *,
-    (actual_days - scheduled_days) as delay_delta,
+select
+    order_id,
+    customer_id,
+    order_at,
+    shipped_at,
+    -- Use the NEW names from your stg_shipments model:
+    actual_shipping_days,
+    scheduled_shipping_days,
+    
+    -- Business Logic: Calculate the delay
+    (actual_shipping_days - scheduled_shipping_days) as delay_delta,
+    
+    -- Business Logic: Flag late deliveries
     case 
-        when actual_days > scheduled_days then true 
+        when actual_shipping_days > scheduled_shipping_days then true 
         else false 
-    end as is_late_delivery
+    end as is_late,
+
+    delivery_status,
+    shipping_mode,
+    sales_amount,
+    profit_amount,
+    order_country,
+    category_name,
+    product_name
+
 from shipments
